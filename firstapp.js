1e1c1c51fefca1274af069e3210ccc70a7c5db9a
mohammad-zytoon <mohamadzaiton@gmail.com>
2021-03-21 14:30:57 +0200

    loops added to the repo

--- firstapp.js
+++ firstapp.js
@@ -10,28 +10,64 @@ if (yes==true) {console.log("horses lover")
 
 var favourteHorsesType = prompt("What is your favourt type of horses exept the other you can found below?")
 
-if (favourteHorsesType === 'arabian') {
+
+while (favourteHorsesType !== 'arabian' && favourteHorsesType !== 'lusitano') {
+    favourteHorsesType = prompt('you have to choose only one of the next two choices arabian and lusitano');
+}
+
+    var numberOfImages = prompt('how many images do you want for the horse you choose?');
+
+    var insertingImages = '';
+   
+    var imagesGroup= '';
+
+    //var insertingLink = '';
+   
+    //var linksGroup= '';
+
+
+   if (favourteHorsesType === 'arabian') {
     alert("good choice");
 
     console.log("arabian horse chosed");
 
     alert("you can learn more about it in the next link");
 
-    document.write("<img src='pictures/arabian-horse-july032020-min.jpg'/>");
-    document.write("<a href='https://nayturr.com/types-of-horses/'>for more information</a>");
+    insertingImages = '<img src="pictures/arabian-horse-july032020-min.jpg" width="250px"/>';
+    
+    //insertingLink = '<a href="https://nayturr.com/types-of-horses/">for more information</a> ';
 
 }
  else if (favourteHorsesType==='lusitano'){ alert("good choice");
           
-          document.write("<img src='pictures/lusitano-horse-july032020-min.jpg'/>");
+    insertingImages ='<img src="pictures/lusitano-horse-july032020-min.jpg" width="250px"/>';
 
          console.log("lusitano horse chosed");
 
          alert("you can learn more about it in the next link");
 
-         document.write("<a href='https://nayturr.com/types-of-horses/'>for more information</a>");}
+        // insertingLink = '<a href="https://nayturr.com/types-of-horses/">for more information</a>';
+    }
 
- else{alert("for more choices we now wrok on developing our website sorry");}
+ //else{alert("for more choices we now wrok on developing our website sorry");}
+     
+     console.log(insertingImages);
+     //console.log(insertingLink);
+     
+     for (var i = 0; i < numberOfImages; i++) {
+       
+        imagesGroup += insertingImages;
+        //linksGroup += insertingLink;
+
+    }
+
+    console.log(imagesGroup);
+    //console.log(linksGroup);
+
+    document.write(imagesGroup);
+   // document.write(linksGroup);
+
+
 
 
 
